bf5981da680e35f6d48a20496ce101a1c3374127
quigt7936 <71140817+quigt7936@users.noreply.github.com>
2020-10-06 10:50:38 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Gives the user the total amount of money in an investing account
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // ask the user the balance, interest and years
+    System.out.println("Please enter a starting balance");
+    double balance = input.nextDouble();
+    System.out.println("Please enter your interest rate");
+    double interest = input.nextDouble();
+    System.out.println("Please enter the number of years you will invest for");
+    double years = input.nextDouble();
+
+    // a for while loop that repeats as many years as given
+    for (int i = 0; i < years; i++) {
+      // finds the new balance 
+      // the interest is / 100 because the interest was in percent form not double form
+      balance = balance + balance * (interest / 100);
+      System.out.println(balance);
+    }
+    // tells the user their new balance
+    System.out.println("Your final balance would be $" + balance);
   }
 }
